1862a15e2139004cdbb3b9acb3c8b9b64eafecf6
Extremoduro12345 <90713077+Extremoduro12345@users.noreply.github.com>
2024-02-05 18:46:03 +0100

    Update settings.py

--- marginalia_paratexta/marginalia_paratexta/settings.py
+++ marginalia_paratexta/marginalia_paratexta/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'marginalia_paratexta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
