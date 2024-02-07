8ed570f094d13e8264b5884ce548d1a41b6efd0b
Extremoduro12345 <90713077+Extremoduro12345@users.noreply.github.com>
2024-02-07 17:29:40 +0100

    Update filters.py

--- marginalia_paratexta/catalog/filters.py
+++ marginalia_paratexta/catalog/filters.py
@@ -1,1 +1,79 @@
+import django_filters
+from .models import BoardGame, Comic, Country, Creation, Genre, KeyWord, Movie, Musica, Novel, Product, TVSerie, Theatre, Videogame
+from django.contrib.contenttypes.models import ContentType
+from django import forms
 
+YEAR_CHOICES = [(str(year), str(year)) for year in range(1930, 2025)]
+
+class YearRangeWidget(forms.widgets.MultiWidget):
+    def __init__(self, attrs=None):
+        year_range = [(year, str(year)) for year in range(1930, 2025)]
+        widgets = (
+            forms.Select(attrs=attrs, choices=year_range),
+            forms.Select(attrs=attrs, choices=year_range),
+        )
+        super().__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            return value
+        return [None, None]  
+
+
+class YearRangeFilter(django_filters.RangeFilter):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('widget', YearRangeWidget())  # Usar nuestro widget personalizado
+        super().__init__(*args, **kwargs)
+
+
+class ProductFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', label='Título')
+    palabras_clave = django_filters.ModelMultipleChoiceFilter(
+        field_name='creation__palabras_clave',
+        queryset=KeyWord.objects.all(),
+        label='Palabras Clave',
+        conjoined=True 
+    )
+    genero = django_filters.ModelMultipleChoiceFilter(
+        field_name='creation__genero',  # Ajusta el campo para reflejar la relación con el género
+        queryset=Genre.objects.all(),
+        label='Género',
+        conjoined=True 
+    )
+
+    paises = django_filters.ModelMultipleChoiceFilter(
+        field_name='creation__paises', 
+        queryset=Country.objects.all(),
+        label='Paises Relacionados',
+        conjoined=True 
+    )
+    
+    creation_type = django_filters.ChoiceFilter(
+        label='Tipo de Creación',
+        method='filter_by_creation_type',
+        choices=[
+            ('movie', 'Movie'),
+            ('tvserie', 'TV Serie'),
+            ('theatre', 'Theatre'),
+            ('musica', 'Musica'),
+            ('boardgame', 'Boardgame'),
+            ('videogame', 'VideoGame'),
+            ('comic', 'Comic'),
+            ('novel', 'Novel'),
+        ]
+    )
+
+
+    publication_year_range = YearRangeFilter(
+        field_name='creation__publication_year',
+        label='Rango de años de publicación'
+    )
+
+    class Meta:
+        model = Product
+        fields = ['title', 'palabras_clave', 'genero', 'creation_type', 'publication_year_range', 'paises' ]
+
+
+    def filter_by_creation_type(self, queryset, name, value):
+        content_type = ContentType.objects.get(model=value)
+        return queryset.filter(creation__polymorphic_ctype_id=content_type.id)
