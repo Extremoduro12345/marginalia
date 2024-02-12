69222e74ca9dd4653e5c74112198f6de10783b6a
Extremoduro12345 <90713077+Extremoduro12345@users.noreply.github.com>
2024-02-12 18:07:32 +0100

    Update urls.py

--- marginalia_paratexta/catalog/urls.py
+++ marginalia_paratexta/catalog/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.index, name='index'),
     path('creations/', views.ProductListView.as_view(), name='creations'),
-    path('transmediality/', views.TransmedialityListView.as_view(), name='transmediality'),
     re_path(r'^creation/(?P<pk>[0-9a-f-]+)$', views.product_detail_view, name='creation-detail'),
     re_path(r'^knot/(?P<pk>[0-9a-f-]+)$', views.knot_detail_view, name='knot-detail'),
 ]
