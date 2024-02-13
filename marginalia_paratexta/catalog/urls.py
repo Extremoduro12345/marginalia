831080a5ff8edf6ee973f418437e9f52311115d8
Extremoduro12345 <90713077+Extremoduro12345@users.noreply.github.com>
2024-02-13 14:58:22 +0100

    Update urls.py

--- marginalia_paratexta/catalog/urls.py
+++ marginalia_paratexta/catalog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('creations/', views.ProductListView.as_view(), name='creations'),
     path('knots/', views.KnotListView.as_view(), name='knots'),
+    path('graph/', views.grafico_barras_view, name='graph'),
     re_path(r'^creation/(?P<pk>[0-9a-f-]+)$', views.product_detail_view, name='creation-detail'),
     re_path(r'^knot/(?P<pk>[0-9a-f-]+)$', views.knot_detail_view, name='knot-detail'),
 ]
